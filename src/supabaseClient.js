0bb5ff7ac2628a2147328ac40211aa565e7c63a6
avtandil <abuashviliavtandil@gmail.com>
2026-05-11 20:27:41 +0400

    fix env quotes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ახლა გასაღებებს წაიღებს .env ფაილიდან
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
